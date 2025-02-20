2b76229544df18170ff5adf261afa43a658a2236
Fernando Alcaraz <fernandoalcaraz115@gmail.com>
2025-02-20 15:42:09 -0600

    Save 2

---
+++ InstaladorVY.py
@@ -0,0 +1,14 @@
+from pytube import YouTube 
+url = input("Ingresa la URL del video que deseas descargar: \n")
+try: 
+    video = YouTube(url)
+except:
+    print("Error de conexión")
+
+download_video = video.streams.get_by_resolution("720p")
+
+try:
+    download_video.download('.')
+except:
+    print("Error al descargar el video")
+print("Video descargado con éxito!")
